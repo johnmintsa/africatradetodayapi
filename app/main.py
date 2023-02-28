ff978f3938747e88e50b39f6c336567fbc8cd987
johnmintsa <mintsajohn@gmail.com>
2023-02-27 20:01:20 -0800

    yes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from .router import post,users, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+
+
 
 
 
@@ -12,7 +15,43 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AfricanTradeToday - Coming Soon</title>
+    <style>
+        body {
+            background-color: #f7f7f7;
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            text-align: center;
+            padding-top: 100px;
+        }
+        h1 {
+            font-size: 48px;
+            color: #0f4d92;
+            margin-bottom: 20px;
+        }
+        p {
+            font-size: 24px;
+            color: #666;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>AfricanTradeToday</h1>
+        <p>Coming Soon</p>
+    </div>
+</body>
+</html>
 
+"""
 origins = ['*']
 app.add_middleware(
     CORSMiddleware,
@@ -26,7 +65,8 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to Africa Trade Today"}
+    return HTMLResponse(html)
+
 
 
 
